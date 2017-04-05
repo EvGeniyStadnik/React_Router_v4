f4e385d49f69c8fdf576be37dbd5f17c54bf7afd
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2017-04-05 22:48:54 +0300

    Redirect by override an existing Route

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ const App = () => (
     <Router>
         <div>
             <Links />
-            <Switch>
-                <Route exact path="/" render={() => <h1>Home</h1>}/>
-                <Route path="/new" render={() => <h1>New</h1>}/>
-                <Redirect from="/old" to="/new"/>
-            </Switch>
+            <Route exact path="/" render={() => <h1>Home</h1>}/>
+            <Route path="/new" render={() => <h1>New</h1>}/>
+            <Route path="/old" render={() => (
+                <Redirect to="/new"/>
+            )} />
         </div>
     </Router>
 );
