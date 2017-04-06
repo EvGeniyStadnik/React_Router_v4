6270a7827b09f02fda81f22b5d4946bddc8f619c
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2017-04-06 12:46:35 +0300

    intercept Route with Prompt Component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import {
     BrowserRouter as Router,
     Route,
     Link,
-    Switch,
-    Redirect
+    Prompt
 } from 'react-router-dom'
 
 import './App.css';
-const loggedin = true;
+
 const Links = () => (
     <div>
         <Link to="/">Home</Link>
-        <Link to="/old/456">Old</Link>
-        <Link to="/new/123">New</Link>
-        <Link to="/protected" >Protected</Link>
+        <Link to="/form">Form</Link>
     </div>
 );
+
+const Home = () => (
+    <h2>Home</h2>
+);
+
+class Form extends React.Component{
+    state = {dirty: false};
+
+    setDirty = () => this.setState({dirty: true});
+    render(){
+        return (
+            <div>
+                <h1>Form</h1>
+                <input type="text" onInput={this.setDirty}/>
+                <Prompt when={this.state.dirty} message='You data will be lost'/>
+            </div>
+        )
+    }
+}
 
 const App = () => (
     <Router>
         <div>
             <Links />
-            <Route exact path="/" render={() => <h1>Home</h1>}/>
-            <Route path="/new/:str" render={({match}) => <h1>New: {match.params.str}</h1>}/>
-            <Route path="/old/:str" render={({match}) => (
-                <Redirect to={`/new/${match.params.str}`}/>
-            )} />
-            <Route path="/protected" render={() => (
-                loggedin
-                ? <h2>Welcome to the protected page</h2>
-                : <Redirect to="/new/Login"/>
-            )}/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/form" component={Form} />
         </div>
     </Router>
 );
